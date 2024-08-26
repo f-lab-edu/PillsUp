647bdb753f32b71f3a0512cf21a6217849145f88
이준영 <junlight94@gmail.com>
2024-08-26 20:53:41 +0900

    feat. 프로젝트 생성

---
+++ PillsUp/PillsUp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PillsUp
+//
+//  Created by Junyoung on 8/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
