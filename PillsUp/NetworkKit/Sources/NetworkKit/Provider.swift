350c6ee2d6eb824003fbe004db36346f6622fd61
이준영 <junlight94@gmail.com>
2024-09-14 23:02:42 +0900

    feat. NetworkKit 작업, XMLParser 작업

---
+++ PillsUp/NetworkKit/Sources/NetworkKit/Provider.swift
@@ -0,0 +1,46 @@
+//
+//  Provider.swift
+//
+//
+//  Created by Junyoung on 9/12/24.
+//
+
+import Foundation
+import Moya
+
+public extension MoyaProvider {
+    static func plain<T: TargetType>() -> MoyaProvider<T> {
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = 10
+        configuration.timeoutIntervalForRequest = 10
+        
+        let session = Session(configuration: configuration)
+        
+        return MoyaProvider<T>(session: session)
+    }
+    
+    func request<T: Decodable>(_ target: Target) async throws -> T {
+        return try await withCheckedThrowingContinuation { continuation in
+            self.request(target) { result in
+                switch result {
+                case .success(let response):
+                    guard (200...300).contains(response.statusCode) else {
+                        continuation.resume(throwing: MoyaError.statusCode(response))
+                        return
+                    }
+                    
+                    do {
+                        let jsonData = try response.data.xmlToBodyJSON()
+                        let result = try JSONDecoder().decode(T.self, from: jsonData)
+                        continuation.resume(returning: result)
+                    } catch {
+                        continuation.resume(throwing: MoyaError.jsonMapping(response))
+                    }
+                    
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+}
---
+++ PillsUp/NetworkKit/Sources/NetworkKit/XmlParser.swift
@@ -0,0 +1,106 @@
+//
+//  XmlParser.swift
+//
+//
+//  Created by Junyoung on 9/13/24.
+//
+
+import Foundation
+
+final class XmlParser: NSObject, XMLParserDelegate {
+    private var currentElement = ""
+    private var currentValue: String?
+    
+    private var currentItem: [String: Any]?
+    private var items = [[String: Any]]()
+    
+    private var isParsingBody = false
+    private var bodyData = [String: Any]()
+    
+    // XMLParser의 델리게이트 메서드 호출 시작
+    func parse(data: Data) -> [String: Any] {
+        let parser = XMLParser(data: data)
+        parser.delegate = self
+        parser.parse()
+        return bodyData
+    }
+    
+    // XML 태그 시작
+    func parser(
+        _ parser: XMLParser,
+        didStartElement elementName: String,
+        namespaceURI: String?,
+        qualifiedName qName: String?,
+        attributes attributeDict: [String : String] = [:]
+    ) {
+        currentElement = elementName
+        currentValue = ""
+        
+        // body 태그가 시작되면 파싱을 시작
+        if elementName == "body" {
+            isParsingBody = true
+        }
+        
+        // item 태그가 시작되면 새로운 아이템 초기화
+        if isParsingBody && elementName == "item" {
+            currentItem = [:]
+        }
+    }
+    
+    // XML 태그 내의 값
+    func parser(
+        _ parser: XMLParser,
+        foundCharacters string: String
+    ) {
+        currentValue? += string.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    
+    // XML 태그 끝
+    func parser(
+        _ parser: XMLParser,
+        didEndElement elementName: String,
+        namespaceURI: String?,
+        qualifiedName qName: String?
+    ) {
+        // body 태그 내부를 파싱할 때만 데이터를 저장
+        if isParsingBody {
+            if let value = currentValue, !value.isEmpty {
+                if elementName == "item" {
+                    // 하나의 item이 끝날 때 items 배열에 추가
+                    if let currentItem = currentItem {
+                        items.append(currentItem)
+                    }
+                    currentItem = nil
+                } else if currentItem != nil {
+                    // item 내부의 key-value 추가
+                    currentItem?[elementName] = value
+                } else {
+                    // item 외부의 데이터를 bodyData에 저장 (numOfRows, pageNo, totalCount 등)
+                    bodyData[elementName] = value
+                }
+            }
+            
+            // items가 끝날 때 items 배열을 저장
+            if elementName == "items" {
+                bodyData["items"] = items
+                items = []  // 다음 items를 위해 초기화
+            }
+            
+            // body 태그가 끝나면 파싱을 종료
+            if elementName == "body" {
+                isParsingBody = false
+            }
+        }
+    }
+}
+
+extension Data {
+    func xmlToBodyJSON() throws -> Data {
+        let parser = XmlParser()
+        let dictionary = parser.parse(data: self)
+        
+        let jsonData = try JSONSerialization.data(withJSONObject: dictionary, options: [])
+        
+        return jsonData
+    }
+}
